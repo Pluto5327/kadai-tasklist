ffa0bb1c49f6934ba827959a80d2813834646481
R F <urawagiants@yahoo.co.jp>
2017-04-22 07:40:59 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
   
-  resources :task, only: [:create, :destroy]
+  resources :task
 end
