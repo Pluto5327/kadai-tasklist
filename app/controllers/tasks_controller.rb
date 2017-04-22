30c5153fa2ec8f25cab06f2d1d6c676532421692
R F <urawagiants@yahoo.co.jp>
2017-04-22 08:27:32 +0000

    a

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all.page(params[:page])
+    @task = Task.all.page(params[:page])
     @task = Task.new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
   
-  resources :task
+  resources :tasks
 end
